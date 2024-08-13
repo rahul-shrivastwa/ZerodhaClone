e9420510f4e18e5519e95da6e8d1c76bb5e111a5
harekrishna <rahulk4kumar8414@gmail.com>
2024-08-13 22:04:59 +0530

    final frontend

---
+++ src/landing_page/support/SupportPage.jsx
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
+
+function supportPage() {
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
+}
+
+export default supportPage;
